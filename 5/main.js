fb7c1915fb92e1804299d02bd4afb4baa2acb783
cros6y6raga <crosbybraga@gmail.com>
2022-12-12 17:05:40 +0300

    lesson-5

---
+++ 5/main.js
@@ -0,0 +1,9 @@
+const strings = ['Alex', 'Donald', 'Bob']
+console.log(strings.sort())
+const strings1 = ['Alex', 'Donald', 'Bob', 100]
+console.log(strings1.sort())
+const strings1 = ['Alex', 'alex', 'Donald', 'Bob', 100]
+
+//1. сортирует строки из коробки т.е. без доп параметров
+//2. сортирует строки типа по алфавиту
+//3. работает мутабельно (сортирует на месте)
