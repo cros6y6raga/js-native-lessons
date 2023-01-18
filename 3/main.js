bf9258ac8e0d58fa784b7b80b35eb6abb574b90d
cros6y6raga <crosbybraga@gmail.com>
2023-01-18 22:18:05 +0300

    lesson 3+

--- 3/main.js
+++ 3/main.js
@@ -1,22 +1,45 @@
-const sm = document.getElementById('small')
+// handler === listener === subscriber => function
+// handler ⊂ callback
+// callback => handler, setTimeout/setInterval, param of arrays method
+// onClick = handler; handler({....})
+// {...} - event, ev, e
 
-const handler1 = () => {
-    alert('Click')
+const sm = document.getElementById('small')
+const md = document.getElementById('medium')
+const bg = document.getElementById('big')
+
+const handler1 = (e) => {
+    e.stopPropagation()
+    if (e.target.tagName === 'BUTTON') {
+        alert(e.target.id)
+    }
+    // console.dir(e.currentTarget)
 }
-const handler2 = () => {
-    alert('Yo')
+
+const handler2 = (e) => {
+    alert(e.currentTarget.id)
+}
+
+const handler3 = (e) => {
+    alert(e.target.id)
 }
 
 // sm.onclick = handler1
 // sm.onclick = handler2
-// sm.onclick = () => {
-//     handler1()
-//     handler2()
-// }
-// sm.onclick = handler2  //удаляем первый обработчик
 
-sm.addEventListener('click', handler1) // #345
-sm.addEventListener('click', handler2)
-sm.removeEventListener('click', handler1) // #666
+// sm.addEventListener('click', handler1)
+// sm.addEventListener('click', handler2)
+// sm.removeEventListener('click', handler1)
+// sm.removeEventListener('click', handler2)
 
-// onClick -> React
+sm.onclick = handler1
+md.onclick = handler2
+bg.onclick = handler3
+
+const handler4 = (e) => {
+    e.preventDefault()
+    alert('hi')
+}
+
+const a = document.getElementById('a')
+a.addEventListener('click', handler4)
