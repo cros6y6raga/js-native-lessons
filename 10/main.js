dff4a2182afeb5eb6ade669a13a053d1a0d59c56
cros6y6raga <crosbybraga@gmail.com>
2023-02-09 20:09:30 +0300

    10

---
+++ 10/main.js
@@ -0,0 +1,9 @@
+//let pr = {}
+
+//button.addEventListener('click', ()=>{})
+//store.subscribe(()=>{})
+
+promise._status = 'pending'; // 'fullfilled', 'rejected'
+promise.then(()=>{})
+promise.catch(()=>{})
+promise.finally()
